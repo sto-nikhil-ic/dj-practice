4994757b8463fcd1e4f2f8517326f3d24ee0bbfc
sto-nikhil-ic <nikhildhital112@gmail.com>
2025-07-13 17:06:02 +0545

    dynamic routing basic

--- pract/urls.py
+++ pract/urls.py
@@ -2,7 +2,7 @@ from django.urls import  path
 from . import views
 urlpatterns = [
     #   path("",views.empty), 
-    path("january",views.index) ,
-    path("febraury",views.empty)
+    path("<month>",views.index) ,
+   
 ]
 
--- pract/views.py
+++ pract/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create you views here.
-def index(request):
-    return HttpResponse("kam garcha ")
-def empty(request):
-    return HttpResponse("this is february")
+def index(request, month):
+    text=None
+    if month == 'january':
+        text=  "this is january"
+    elif month == 'february':
+        text="this is february"
+    else:
+         return HttpResponseNotFound("not found")
+    return HttpResponse(text)
+    
+    
